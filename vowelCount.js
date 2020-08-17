86fa0cb57bbcfff163c1ad566105cd180224deb0
mansurmansur <mansurmansur@trentu.ca>
2020-08-17 02:47:01 -0400

    This program is about vowel count. a function which takes in string and counts numbers of vowels in it

---
+++ vowelCount.js
@@ -0,0 +1,38 @@
+/**
+ * Author: Mansur Mansur
+ * 
+ * Description: This program contains a function that takes in a string and counts vowels
+ *              😁
+ */
+
+ //global variable 
+ const vowel = ['a','e', 'i','o','u']; //vowel arrays contains all the vowels
+
+
+ //function name: vowelCount()
+ //parameter: str
+ //algorithmn:
+ //         1. create count variable
+ //         2. loop through the string and increment count if their is vowel
+ //         3. return count
+ function vowelCount(str){
+     //variable
+     let count = 0;
+
+     //loop through the string
+     for (let i= 0; i< str.length; i++) {
+        for (let j = 0; j < vowel.length; j++) {
+            if(str[i] == vowel[j]){
+                count++;
+                break;
+            }
+        }
+     }
+
+     //return the value
+     return count;
+ }
+
+
+ //invoke the method
+ console.log("There are "+ vowelCount("i am Mansur Mansur.") +" vowels in the string.")
